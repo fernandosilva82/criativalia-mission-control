01cb140b9416b47708b10c9f5d1f4e1f213a0520
Criativalia Deploy <deploy@criativalia.com>
2026-03-29 19:56:11 +0800

    fix: Adiciona rota raiz / para servir index.html corretamente

--- criativalia-mission-control/control-plane/src/api.js
+++ criativalia-mission-control/control-plane/src/api.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Rota raiz - Dashboard
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 // Página Financial
 app.get('/financial', (req, res) => {
     res.sendFile(path.join(__dirname, '../src/pages/financial.html'));
